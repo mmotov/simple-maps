49f9e764481b9d58684ee7873f362888b2532378
Maksym <motov.maxim@gmail.com>
2020-03-22 12:43:10 +0200

    starter code for the project provided by Coursera

---
+++ src/main/java/simpleMaps/App.java
@@ -0,0 +1,13 @@
+package simpleMaps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
